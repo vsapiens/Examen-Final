cb83ba76a3bf5e776614fcc14c5afebfe4edd307
Alfredo Salazar <>
2020-02-04 16:48:47 -0600

    Added CORS

--- moviedex-server/index.js
+++ moviedex-server/index.js
@@ -6,9 +6,19 @@ let { DATABASE_URL, PORT } = require( './config' );
 
 let app = express();
 
-let server;
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+	if (req.method === "OPTIONS") {
+		return res.send(204);
+	}
+	next();
+});
 
 /* Tu código va aquí */
+
+let server;
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
